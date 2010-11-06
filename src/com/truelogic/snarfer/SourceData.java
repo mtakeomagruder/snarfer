65208a56fe3bfc5cf67a4b2ec37181183692ccad
David Steele <david@thelabyrinth.net>
2010-11-06 20:20:21 +0000

    Added documentation
    git-svn-id: file:///data/svn/code/utility/snarfer/trunk@625 7bf47e22-5cfc-0310-b71d-91f6c37fd135

--- src/com/truelogic/snarfer/SourceData.java
+++ src/com/truelogic/snarfer/SourceData.java
@@ -2,16 +2,32 @@ package com.truelogic.snarfer;
 
 import java.util.*;
 
+/***********************************************************************************************************************
+* Contains information about each news source listed in the INI file.
+* 
+* @author David Steele
+***********************************************************************************************************************/
 public class SourceData 
 {
-    private String strID;
-    private Vector<String> strURLs = new Vector<String>();
-    private String strName;
-    private int iImageWidthMin;
-    private int iAspectRatioMax;
-    private int iArticleSizeMin;
-    private int iArticleChunkSizeMin;
+    private String strID;                                   // The source ID (short text string)
+    private Vector<String> strURLs = new Vector<String>();  // URLs for the RSS feeds
+    private String strName;                                 // The source name
+    private int iImageWidthMin;                             // The minimum width for the article image 
+    private int iAspectRatioMax;                            // Max image aspect ratio
+    private int iArticleSizeMin;                            // Minimum size for the article text
+    private int iArticleChunkSizeMin;                       // Minimum chunk size for each section of the article
     
+    /*******************************************************************************************************************
+    * Initializes the SourceData object.
+    * 
+    * @param strID                 The source ID (short text string - letters and underscore only)
+    * @param strURLS               A list of RSS URLs for the source
+    * @param strName               The source name (for display purposes - all characters allowed)
+    * @param iImageWidthMin        The minimum width for the article image
+    * @param iAspectRatioMax       Max image aspect ratio
+    * @param iArticleSizeMin       Minimum size for the article text
+    * @param iArticleChunkSizeMin  Minimum chunk size for each section of the article
+    *******************************************************************************************************************/
     public SourceData(String strID, Vector<String> strURLs, String strName, int iImageWidthMin, int iAspectRatioMax,
                       int iArticleSizeMin, int iArticleChunkSizeMin)
     {
@@ -24,36 +40,57 @@ public class SourceData
         this.iArticleChunkSizeMin = iArticleChunkSizeMin;
     }
 
+    /*******************************************************************************************************************
+    * @return The source ID (for internal use).
+    *******************************************************************************************************************/
     public String getID() 
     {
         return(strID);
     }
 
+    /*******************************************************************************************************************
+    * @return The list of RSS URLs for the source.
+    *******************************************************************************************************************/
     public Vector<String> getURLs() 
     {
         return(strURLs);
     }
 
+    /*******************************************************************************************************************
+    * @return The source name (for display).
+    *******************************************************************************************************************/
     public String getName() 
     {
         return(strName);
     }
 
+    /*******************************************************************************************************************
+    * @return The minimum article image width (or height if portrait orientation). 
+    *******************************************************************************************************************/
     public int getImageWidthMin() 
     {
         return(iImageWidthMin);
     }
 
+    /*******************************************************************************************************************
+    * @return The maximum aspect ratio for the article image (long side / short side) 
+    *******************************************************************************************************************/
     public int getAspectRatioMax() 
     {
         return(iAspectRatioMax);
     }
 
+    /*******************************************************************************************************************
+    * @return The minimum size for the article (all chunks together) 
+    *******************************************************************************************************************/
     public int getArticleSizeMin() 
     {
         return(iArticleSizeMin);
     }
 
+    /*******************************************************************************************************************
+     * @return The minimum size for an article chunk 
+     *******************************************************************************************************************/
     public int getArticleChunkSizeMin() 
     {
         return(iArticleChunkSizeMin);
