9e0abb301b2afda91c045c9694a432ff48de9cdc
David Steele <david@thelabyrinth.net>
2010-11-26 22:21:47 +0000

    Added comments
    git-svn-id: file:///data/svn/code/utility/snarfer/trunk@668 7bf47e22-5cfc-0310-b71d-91f6c37fd135

--- src/com/truelogic/snarfer/db/DbArticle.java
+++ src/com/truelogic/snarfer/db/DbArticle.java
@@ -1,107 +1,166 @@
 package com.truelogic.snarfer.db;
 
+/***********************************************************************************************************************
+* Represents an article loaded from the database (denormalized).
+* 
+* @author David Steele
+***********************************************************************************************************************/
 public class DbArticle 
 {
-    private int iID;
-    private int iSourceUrlID;
-    private String strRandomID;
-    private int iTier;
-    private int iTextSize;
-    private String strTextHash;
-    private String strTextUrl;
-    private String strText;
-    private int iImageID;
-    private int iImageSize;
-    private String strImageHash;
-    private String strImageUrl;
-    private byte[] tyImage;
-    private int iBatchID;
-    
-    public DbArticle(int iID, int iSourceUrlID, String strRandomID, int iTier, int iTextSize, String strTextHash, 
-                     String strTextUrl, String strText, int iImageID, int iImageSize, String strImageHash,
-                     String strImageUrl, byte[] tyImage, int iBatchID)
+    private int iID;                // Source DB ID
+    private int iSourceUrlID;       // Source URL DB ID
+    private String strRandomID;     // Source random DB text ID
+    private int iTier;              // Article Tier (1 for primary, 2 for all else)
+    private String strTextHash;     // MD5 hash of article text
+    private String strTextUrl;      // Article URL
+    private String strText;         // Article text
+    private int iImageID;           // Image DB ID
+    private String strImageHash;    // MD5 hash of image
+    private String strImageUrl;     // Image URL
+    private byte[] tyImage;         // JPEG image stored in a byte array
+    private int iBatchID;           // Batch DB ID
+
+    /*******************************************************************************************************************
+    * Initializes the DbArticle.
+    * 
+    * @param iID           Source DB ID          
+    * @param iSourceUrlID  Source URL DB ID
+    * @param strRandomID   Source random DB text ID
+    * @param iTier         Article Tier (1 for primary, 2 for all else)
+    * @param strTextHash   MD5 hash of article text
+    * @param strTextURL    Article URL
+    * @param strText       Article text
+    * @param iImageID      Image DB ID
+    * @param strImageHash  MD5 hash of image
+    * @param strImageUrl   Image URL
+    * @param tyImage       JPEG image stored in a byte array
+    * @param iBatchID      Batch DB ID
+    *******************************************************************************************************************/
+    public DbArticle(int iID, int iSourceUrlID, String strRandomID, int iTier, String strTextHash, String strTextUrl, 
+                     String strText, int iImageID, String strImageHash, String strImageUrl, byte[] tyImage, 
+                     int iBatchID)
     {
         this.iID = iID;
         this.iSourceUrlID = iSourceUrlID;
         this.strRandomID = strRandomID;
         this.iTier = iTier;
-        this.iTextSize = iTextSize;
         this.strTextHash = strTextHash;
         this.strTextUrl = strTextUrl;
         this.strText = strText;
         this.iImageID = iImageID;
-        this.iImageSize = iImageSize;
         this.strImageHash = strImageHash;
         this.strImageUrl = strImageUrl;
         this.tyImage = tyImage;
         this.iBatchID = iBatchID;
     }
     
+    /*******************************************************************************************************************
+    * @return Source DB ID
+    *******************************************************************************************************************/
     public int getID() 
     {
         return(iID);
     }
 
+    /*******************************************************************************************************************
+    * @return Source URL DB ID
+    *******************************************************************************************************************/
     public int getSourceUrlID() 
     {
         return(iSourceUrlID);
     }
 
+    /*******************************************************************************************************************
+    * @return Source random DB text ID
+    *******************************************************************************************************************/
     public String getRandomID() 
     {
         return(strRandomID);
     }
 
+    /*******************************************************************************************************************
+    * @return Article Tier (1 for primary, 2 for all else)
+    *******************************************************************************************************************/
     public int getTier() 
     {
         return(iTier);
     }
 
+    /*******************************************************************************************************************
+    * @return Article size in bytes
+    *******************************************************************************************************************/
     public int getTextSize() 
     {
-        return(iTextSize);
+        return(strText.length());
     }
 
+    /*******************************************************************************************************************
+    * @return MD5 hash of article text
+    *******************************************************************************************************************/
     public String getTextHash() 
     {
         return(strTextHash);
     }
 
+    /*******************************************************************************************************************
+    * @return Article URL
+    *******************************************************************************************************************/
     public String getTextUrl() 
     {
         return(strTextUrl);
     }
 
+    /*******************************************************************************************************************
+    * @return Article text
+    *******************************************************************************************************************/
     public String getText() 
     {
         return(strText);
     }
 
+    /*******************************************************************************************************************
+    * @return Image DB ID
+    *******************************************************************************************************************/
     public int getImageID() 
     {
         return(iImageID);
     }
 
+    /*******************************************************************************************************************
+    * @return Size of image in bytes
+    *******************************************************************************************************************/
     public int getImageSize() 
     {
-        return(iImageSize);
+        return(tyImage.length);
     }
 
+    /*******************************************************************************************************************
+    * @return MD5 hash of image
+    *******************************************************************************************************************/
     public String getImageHash() 
     {
         return(strImageHash);
     }
 
+    /*******************************************************************************************************************
+    * @return Image URL
+    *******************************************************************************************************************/
     public String getImageUrl() 
     {
         return(strImageUrl);
     }
 
+    /*******************************************************************************************************************
+    * @return JPEG image stored in a byte array
+    *******************************************************************************************************************/
     public byte[] getImage() 
     {
         return(tyImage);
     }
 
+    /*******************************************************************************************************************
+    * @return Batch DB ID 
+    *******************************************************************************************************************/
     public int getBatchID() 
     {
         return(iBatchID);
--- src/com/truelogic/snarfer/db/DbSource.java
+++ src/com/truelogic/snarfer/db/DbSource.java
@@ -3,15 +3,30 @@ package com.truelogic.snarfer.db;
 // Java imports
 import java.util.Vector;
 
+/***********************************************************************************************************************
+* Represents a news source loaded from the database (denormalized).
+* 
+* @author David Steele
+***********************************************************************************************************************/
 public class DbSource 
 {
-    private int iID;
-    private String strTextID;
-    private String strName;
-    private int iUrlID;
-    private String strUrl;
-    private Vector<DbArticle> oArticles;
+    private int iID;                        // Synthetic DB ID     
+    private String strTextID;               // Source Text ID
+    private String strName;                 // Source Name (for display)
+    private int iUrlID;                     // Source URL DB ID
+    private String strUrl;                  // Source URL
+    private Vector<DbArticle> oArticles;    // List of source articles (for the current URL)
     
+    /*******************************************************************************************************************
+    * Initializes the DbSource.
+    * 
+    * @param iID        Synthetic DB ID
+    * @param strTextID  Source Text ID
+    * @param strName    Source Name (for display)
+    * @param iUrlID     Source URL DB ID
+    * @param strURL     Source URL
+    * @param oArticles  List of source articles (for the current URL)
+    *******************************************************************************************************************/
     public DbSource(int iID, String strTextID, String strName, int iUrlID, String strUrl, 
                     Vector<DbArticle> oArticles)
     {
@@ -23,31 +38,49 @@ public class DbSource
         this.oArticles = oArticles;
     }
     
+    /*******************************************************************************************************************
+    * @return Synthetic DB ID
+    *******************************************************************************************************************/
     public int getID() 
     {
         return(iID);
     }
 
+    /*******************************************************************************************************************
+    * @return Source Text ID
+    *******************************************************************************************************************/
     public String getTextID() 
     {
         return(strTextID);
     }
 
+    /*******************************************************************************************************************
+    * @return Source Name (for display)
+    *******************************************************************************************************************/
     public String getName() 
     {
         return(strName);
     }
 
+    /*******************************************************************************************************************
+    * @return Source URL DB ID
+    *******************************************************************************************************************/
     public int getUrlID() 
     {
         return(iUrlID);
     }
 
+    /*******************************************************************************************************************
+    * @return Source URL
+    *******************************************************************************************************************/
     public String getUrl() 
     {
         return(strUrl);
     }
 
+    /*******************************************************************************************************************
+    * @return List of source articles (for the current URL)
+    *******************************************************************************************************************/
     public Vector<DbArticle> getArticles() 
     {
         return(oArticles);
