05d987f2cf2abfc13a89cb6389828d8c94581264
David Steele <david@thelabyrinth.net>
2010-11-26 22:56:52 +0000

    Added comments
    git-svn-id: file:///data/svn/code/utility/snarfer/trunk@672 7bf47e22-5cfc-0310-b71d-91f6c37fd135

--- src/com/truelogic/snarfer/db/Db.java
+++ src/com/truelogic/snarfer/db/Db.java
@@ -6,10 +6,18 @@ import java.sql.*;
 // Project imports
 import com.truelogic.snarfer.config.*;
 
+/***********************************************************************************************************************
+* Db class to handle database connections.
+* 
+* @author David Steele
+***********************************************************************************************************************/
 public class Db 
 {
-    private Connection oDb = null;
+    private Connection oDb = null;  // The JDBC database connection
     
+    /*******************************************************************************************************************
+    * Initializes Db and connects to the JDBC database.
+    *******************************************************************************************************************/
     public Db(ConfigDb oConfigDb) throws ClassNotFoundException, SQLException
     {
         Class.forName(oConfigDb.getDriver());
@@ -18,6 +26,9 @@ public class Db
         oDb.setAutoCommit(false);
     }
     
+    /*******************************************************************************************************************
+    * @return JDBC database connection
+    *******************************************************************************************************************/
     protected Connection getDb()
     {
         return(oDb);
