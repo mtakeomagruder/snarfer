31241eb4f9e6aa21a57a34ea2b992106c1f5ec47
David Steele <david@thelabyrinth.net>
2010-12-10 21:57:00 +0000

    Added comments
    git-svn-id: file:///data/svn/code/utility/snarfer/trunk@683 7bf47e22-5cfc-0310-b71d-91f6c37fd135

--- src/com/truelogic/snarfer/config/ConfigDb.java
+++ src/com/truelogic/snarfer/config/ConfigDb.java
@@ -1,12 +1,25 @@
 package com.truelogic.snarfer.config;
 
+/***********************************************************************************************************************
+* Contains information about the database.
+* 
+* @author David Steele
+***********************************************************************************************************************/
 public class ConfigDb 
 {
     private String strDriver;           // JDBC driver
     private String strConnect;          // JDBC connect string
-    private String strUser;             // The DB user name
-    private String strPassword;         // The user password (may be missing or blank)
+    private String strUser;             // DB user name
+    private String strPassword;         // User password (may be missing or blank)
     
+    /*******************************************************************************************************************
+    * Initializes ConfigDb.
+    * 
+    * @param strDriver    JDBC driver
+    * @param strConnect   JDBC connect string
+    * @param strUser      DB user name
+    * @param strPassword  User password (may be missing or blank)
+    *******************************************************************************************************************/
     public ConfigDb(String strDriver, String strConnect, String strUser, String strPassword)
     {
         this.strDriver = strDriver;
@@ -15,21 +28,33 @@ public class ConfigDb
         this.strPassword = strPassword;
     }
 
+    /*******************************************************************************************************************
+    * @return JDBC driver
+    *******************************************************************************************************************/
     public String getDriver() 
     {
         return(strDriver);
     }
 
+    /*******************************************************************************************************************
+    * @return JDBC connect string
+    *******************************************************************************************************************/
     public String getConnect() 
     {
         return(strConnect);
     }
 
+    /*******************************************************************************************************************
+    * @return DB user name
+    *******************************************************************************************************************/
     public String getUser() 
     {
         return(strUser);
     }
 
+    /*******************************************************************************************************************
+    * @return User password (may be missing or blank)
+    *******************************************************************************************************************/
     public String getPassword() 
     {
         return(strPassword);
